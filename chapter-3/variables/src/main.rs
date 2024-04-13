e32c35dd9138a9ef870d4bbd81d59128c0279ec2
eshan-sharma <43044334+Eshan-Sharma@users.noreply.github.com>
2024-04-13 18:56:30 +0530

    comment immutable variable

--- chapter-3/variables/src/main.rs
+++ chapter-3/variables/src/main.rs
@@ -5,10 +5,10 @@
 //     println!("The value of x is : {x}");
 // }
 fn main() {
-    let mut x = 5;
+    let mut x = 5; //mutable variable and can be changed
     println!("The value of x is : {x}");
     x = 6;
     println!("The value of x is : {x}");
-    const THREE_HOURS_IN_SECONDS: u32 = 3 * 60 * 60;
+    const THREE_HOURS_IN_SECONDS: u32 = 3 * 60 * 60; //const
     println!("3 hours in seconds {}", THREE_HOURS_IN_SECONDS);
 }
