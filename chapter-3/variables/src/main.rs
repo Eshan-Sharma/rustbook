1f65c5df6b8d11b82cbd4a62b15c2cc51f6ff579
eshan-sharma <43044334+Eshan-Sharma@users.noreply.github.com>
2024-04-13 18:49:40 +0530

    learning mutable and immutable variables

---
+++ chapter-3/variables/src/main.rs
@@ -0,0 +1,14 @@
+// fn main() {
+//     let x = 5;
+//     println!("The value of x is : {x}");
+//     x = 6; // immutable variable it throws an error
+//     println!("The value of x is : {x}");
+// }
+fn main() {
+    let mut x = 5;
+    println!("The value of x is : {x}");
+    x = 6;
+    println!("The value of x is : {x}");
+    const THREE_HOURS_IN_SECONDS: u32 = 3 * 60 * 60;
+    println!("3 hours in seconds {}", THREE_HOURS_IN_SECONDS);
+}
