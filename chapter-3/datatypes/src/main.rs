ab65f79f246b54155ba82486b4e4be35689f1c4f
eshan-sharma <43044334+Eshan-Sharma@users.noreply.github.com>
2024-04-16 20:09:15 +0530

    learning datatypes in rust

---
+++ chapter-3/datatypes/src/main.rs
@@ -0,0 +1,22 @@
+fn main() {
+    println!("Hello, world! just printing data types");
+    let x: f32 = 3.02; //f32
+    println!("x is {}", x);
+
+    let y = 4.01; //f64
+    println!("y is {}", y);
+
+    let sum = 5 + 10;
+    println!("sum is {sum}");
+
+    let diff = 34.2 - 21.02;
+    println!("diff is {diff}");
+
+    let quotient = 543.21 / 89.1;
+    println!("quotient is {quotient}");
+    let truncated = -1 / 9;
+    println!("truncated is {truncated}");
+
+    let remainder = 10 % 3;
+    println!("remainder is {remainder}");
+}
