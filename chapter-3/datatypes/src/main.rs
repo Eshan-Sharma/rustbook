a36e03269f688d0a6a7cd28a0094ee1f2d6b3f05
eshan-sharma <43044334+Eshan-Sharma@users.noreply.github.com>
2024-04-16 20:33:20 +0530

    learning datatypes in rust (arrays)

--- chapter-3/datatypes/src/main.rs
+++ chapter-3/datatypes/src/main.rs
@@ -7,16 +7,29 @@ fn main() {
     println!("y is {}", y);
 
     let sum = 5 + 10;
-    println!("sum is {sum}");
+    println!("sum is {sum}"); //int
 
     let diff = 34.2 - 21.02;
-    println!("diff is {diff}");
+    println!("diff is {diff}"); //float - this shows an interesting property as well of floating point error
 
     let quotient = 543.21 / 89.1;
-    println!("quotient is {quotient}");
+    println!("quotient is {quotient}"); //float divided by float
     let truncated = -1 / 9;
-    println!("truncated is {truncated}");
+    println!("truncated is {truncated}"); // int divided by int
 
     let remainder = 10 % 3;
     println!("remainder is {remainder}");
+
+    let c: char = 'q';
+    println!("character is {c}"); //char
+
+    let babe: (u32, char, f32) = (123, 'd', 4.54);
+    println!("babe is {},{},{}", babe.0, babe.1, babe.2);
+
+    let arr = [1, 2, 4, 5, 8];
+    let months = [
+        "jan", "feb", "mar", "apr", "may", "jun", "jul", "aug", "sep", "oct", "nov", "dec",
+    ];
+    let arr1 = [i32; 5];
+    println!("array is ");
 }
