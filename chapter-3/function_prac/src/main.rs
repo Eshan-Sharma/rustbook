6427f29f86760ad53b31134f3f40763d863e55ec
eshan-sharma <43044334+Eshan-Sharma@users.noreply.github.com>
2024-04-16 20:51:09 +0530

    params function

--- chapter-3/function_prac/src/main.rs
+++ chapter-3/function_prac/src/main.rs
@@ -1,6 +1,7 @@
 fn main() {
     println!("Hello, world!");
-    print_name();
+    //print_name();
+    sum(3, 4);
 }
 
 fn print_name() {
@@ -8,4 +9,11 @@ fn print_name() {
     let mut name = String::new();
     std::io::stdin().read_line(&mut name).expect("Enter name");
     println!("Your name is {name}");
+}
+
+fn sum(x: u32, y: u32) {
+    //unsigned 32
+    //the data types are defined here and not while passing the variables,
+    //this is similar to other languages like java (for own understanding)
+    println!("x + y is {}", x + y);
 }
