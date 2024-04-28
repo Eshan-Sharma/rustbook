1210376851ad8bae2dc7682f68ba22ad5fbdc031
eshan-sharma <43044334+Eshan-Sharma@users.noreply.github.com>
2024-04-28 14:05:34 +0530

    guessing game rustfmt formatting

--- chapter-2/guessing_game2/src/main.rs
+++ chapter-2/guessing_game2/src/main.rs
@@ -1,3 +1,33 @@
+use rand::Rng;
+use std::{cmp::Ordering, io};
 fn main() {
-    println!("Hello, world!");
+    println!("Guess a number!");
+    let secret_number = rand::thread_rng().gen_range(1..=100);
+    println!("The secret number is: {secret_number}");
+
+    loop {
+        println!("Please input your guess");
+
+        let mut guess = String::new();
+        io::stdin()
+            .read_line(&mut guess)
+            .expect("Failed to read line");
+        let guess: u32 = match guess.trim().parse() {
+            Ok(num) => num,
+            Err(_) => {
+                println!("Please enter a number");
+                continue;
+            }
+        };
+        println!("Your guess is: {}", guess);
+
+        match guess.cmp(&secret_number) {
+            Ordering::Less => println!("Too small"),
+            Ordering::Equal => {
+                println!("You win");
+                break;
+            }
+            Ordering::Greater => println!("Too big"),
+        }
+    }
 }
