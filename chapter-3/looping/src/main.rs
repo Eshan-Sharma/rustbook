14810de2a14292bdda8593819de845f3d9d4dec1
eshan-sharma <43044334+Eshan-Sharma@users.noreply.github.com>
2024-04-29 18:11:36 +0530

    loop label

--- chapter-3/looping/src/main.rs
+++ chapter-3/looping/src/main.rs
@@ -17,5 +17,25 @@ fn main() {
     };
     println!("The result is {result}");
     println!("---------------------------------");
-   
+    multiple_loop();
+}
+
+fn multiple_loop() {
+    let mut count = 0;
+    'counting_up: loop {
+        println!("count = {count}");
+        let mut remaining = 10;
+        loop {
+            println! {"remaining is {remaining}"};
+            if (remaining == 9) {
+                break;
+            }
+            if count == 2 {
+                break 'counting_up;
+            }
+            remaining -= 1;
+        }
+        count += 1;
+    }
+    println!("End count = {count}");
 }
