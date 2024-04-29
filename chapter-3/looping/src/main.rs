120c297cf8d8c26fa2d57cfe39048eea5c21fe9d
eshan-sharma <43044334+Eshan-Sharma@users.noreply.github.com>
2024-04-29 18:06:38 +0530

    return a value from loop

--- chapter-3/looping/src/main.rs
+++ chapter-3/looping/src/main.rs
@@ -1,10 +1,21 @@
 fn main() {
     let mut x = 1;
-    loop{
+    loop {
         println!("again!");
-        x+=1; //Check to make sure the loop is not infinite
-        if(x>3){
+        x += 1; //Check to make sure the loop is not infinite
+        if (x > 3) {
             break;
         }
     }
+    println!("---------------------------------");
+    let mut counter = 0;
+    let result = loop {
+        counter += 1;
+        if counter == 10 {
+            break counter * 2;
+        }
+    };
+    println!("The result is {result}");
+    println!("---------------------------------");
+   
 }
