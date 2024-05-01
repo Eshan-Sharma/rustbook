1bf48a490d924c3c0eb26bdcbce4ec8d65129a43
eshan-sharma <43044334+Eshan-Sharma@users.noreply.github.com>
2024-05-01 09:52:45 +0530

    ownership passing around in functions

--- chapter-4/src/main.rs
+++ chapter-4/src/main.rs
@@ -20,15 +20,41 @@ fn main() {
     let str1 = String::from("string value");
     let str2 = str1;
     //println!("{str1}");//This will return an error since the pointer to the value of str1 has been moved to the str2
-    println!("{str2}"); //shallow copy
+    println!("{str2}"); //shallow copy, but since str1 pointer is "moved" to str2. The process is "move"
+                        //Deep copies are not automatically created
+                        //this type of copy is inexpensive in terms of runtime performance
 
-    //
     let str_to_be_copied = String::from("copy me!");
     let str_which_copied = str_to_be_copied.clone(); //Creates a deep copy, but this has to be expicitly mentioned as "clone"
     println!(
-        "string to be copied is: {} and String copied is: {}",
+        "string to be copied is: {}\n and String copied is: {}",
         str_to_be_copied, str_which_copied
     );
+    ownership();
 }
 
 //scope end s is not valid
+
+fn ownership() {
+    let str = String::from("hwllo"); //str is here
+    takes_ownership(str); //str is passed, ownership is passed'
+                          //println!("{str}");//This will give error since str no longer valid here
+    let x = 5;
+    makes_copy(x); //copy of x is passed
+    println!("x after function: {x}");
+
+    let str1 = String::from("take and give back");
+    let str2 = gives_ownership(str1);
+    println!("str2 after: {str2}");
+    //println!("str1 after: {str1}");//gives error\
+}
+fn takes_ownership(some_string: String) {
+    println!("{}", some_string);
+}
+fn makes_copy(some_integer: i32) {
+    println!("{}", some_integer);
+}
+fn gives_ownership(sone_string: String) -> String {
+    println!("Some string inside gives back ownership: {sone_string}");
+    sone_string
+}
