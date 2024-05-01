f63d9ef02b5e74f96db84e72264cf851da823000
eshan-sharma <43044334+Eshan-Sharma@users.noreply.github.com>
2024-05-01 15:28:59 +0530

    struct implementation

--- chapter-5/structs/src/main.rs
+++ chapter-5/structs/src/main.rs
@@ -3,7 +3,18 @@ struct User {
     name: String,
     email: String,
 }
+//tuple struct
+struct Colour(i32, i32, i32);
 
+struct Rectangle {
+    width: i32,
+    height: i32,
+}
+impl Rectangle {
+    fn area(&self) -> i32 {
+        self.height * self.width
+    }
+}
 fn main() {
     let user = User {
         active: true,
@@ -14,4 +25,12 @@ fn main() {
         "name is {},email is {} and the user is active:{}",
         user.name, user.email, user.active
     );
+
+    // let black: Colour = Colour(0, 0, 0);
+    // println!("{}", black);
+    let rect = Rectangle {
+        width: 12,
+        height: 12,
+    };
+    println!("{}", rect.area());
 }
