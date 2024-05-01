692c51578e009f62d72a69d61af5709b7147ad7c
eshan-sharma <43044334+Eshan-Sharma@users.noreply.github.com>
2024-05-01 08:54:09 +0530

    learning ownership

---
+++ chapter-4/src/main.rs
@@ -0,0 +1,8 @@
+fn main() {
+    // s will give a warning at compile time, use _s.
+    // _ tells the compiler that it is intentional, compiler need not worry about it (we know what we are doing)
+    let s = "hello"; //string literal, value is hard coded into the text of program.
+                     //s is valid here
+}
+
+//scope end s is not valid
