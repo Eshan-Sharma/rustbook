6b86bd3b5728384df6d334ea55d17b3048ee2835
eshan-sharma <43044334+Eshan-Sharma@users.noreply.github.com>
2024-05-02 16:14:36 +0530

    pattern matching in enum

--- chapter-5/enums/src/main.rs
+++ chapter-5/enums/src/main.rs
@@ -16,4 +16,20 @@ fn main() {
         let home = IpAddr::V4(127, 0, 0, 0);
         let looping = IpAddr::V6(String::from("::1"));
     }
+    {
+        enum Coin {
+            Penny,
+            Nickel,
+            Dime,
+            Quarter,
+        }
+        fn value_in_cents(coin: Coin) -> u8 {
+            match coin {
+                Coin::Penny => 1,
+                Coin::Nickel => 5,
+                Coin::Dime => 10,
+                Coin::Quarter => 25,
+            }
+        }
+    }
 }
