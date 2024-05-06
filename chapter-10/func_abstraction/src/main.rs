47b7e2c1d4d357cd25eadb598545966a99c64180
eshan-sharma <43044334+Eshan-Sharma@users.noreply.github.com>
2024-05-06 07:17:04 +0530

    abstraction of function logic

--- chapter-10/func_abstraction/src/main.rs
+++ chapter-10/func_abstraction/src/main.rs
@@ -1,19 +1,19 @@
 fn main() {
     let number_list = vec![34, 50, 25, 100, 65];
-    let mut largest = &number_list[0];
-
-    for number in &number_list {
-        if number > largest {
-            largest = number;
-        }
-    }
-    println!("Largest number is {}", largest);
+    let largest_num = largest(&number_list);
+    println!("Largest number is {}", largest_num);
 
     let number_list = vec![102, 34, 6000, 89, 54, 2, 43, 8];
-    let mut largest = &number_list[0];
-    for number in &number_list {
-        if number > largest {
-            largest = number;
+    let largest_num = largest(&number_list);
+    println!("Largest number is {}", largest_num);
+}
+
+fn largest(list: &Vec<i32>) -> &i32 {
+    let mut largest_num: &i32 = &list[0];
+    for number in list {
+        if number > largest_num {
+            largest_num = number;
         }
     }
+    largest_num
 }
