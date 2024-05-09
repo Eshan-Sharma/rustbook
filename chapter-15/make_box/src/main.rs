6a75da51c71a5d5a6489002198dc2d53f0fd4b54
eshan-sharma <43044334+Eshan-Sharma@users.noreply.github.com>
2024-05-09 11:57:46 +0530

    Rc implementation

--- chapter-15/make_box/src/main.rs
+++ chapter-15/make_box/src/main.rs
@@ -1,5 +1,10 @@
+// enum List {
+//     Cons(i32, Box<List>), // Rust knows how much space does Box<T> takes since it is a pointer
+//     Nil,
+// }
+use std::rc::Rc;
 enum List {
-    Cons(i32, Box<List>), // Rust knows how much space does Box<T> takes since it is a pointer
+    Cons(i32, Rc<List>),
     Nil,
 }
 fn main() {
@@ -12,9 +17,15 @@ fn main() {
     //     Nil,
     // }
 
-    let list = List::Cons(
-        1,
-        Box::new(List::Cons(2, Box::new(List::Cons(3, Box::new(List::Nil))))),
-    );
+    //Box implementation
+    // let list = List::Cons(
+    //     1,
+    //     Box::new(List::Cons(2, Box::new(List::Cons(3, Box::new(List::Nil))))),
+    // );
     //Boxes provide only the indirection and heap allocation; they don’t have any other special capabilities, like those we’ll see with the other smart pointer types.
+
+    //Rc implementation
+    let a = Rc::new(List::Cons(5, Rc::new(List::Cons(10, Rc::new(List::Nil)))));
+    let b = List::Cons(3, Rc::clone(&a));
+    let c = List::Cons(4, Rc::clone(&a)); // value moved error
 }
