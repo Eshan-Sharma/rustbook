921bb0c7127f35ddb6112de7b12eac24717d2468
eshan-sharma <43044334+Eshan-Sharma@users.noreply.github.com>
2024-05-09 18:40:30 +0530

    move a variable used

--- chapter-16/threads_learn/src/main.rs
+++ chapter-16/threads_learn/src/main.rs
@@ -14,4 +14,12 @@ fn main() {
         thread::sleep(Duration::from_millis(1));
     }
     handle.join().unwrap();
+
+    let v = vec![1, 2, 3];
+
+    let handle = thread::spawn(move || {
+        println!("Here's a vector: {:?}", v);
+    });
+
+    handle.join().unwrap();
 }
